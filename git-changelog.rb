7fa7ce854b51b03991cd20213d5e13009ed37d25
Aldrin John D'Souza <aldrin@users.noreply.github.com>
2018-02-17 14:54:42 -0500

    git-changelog 0.3.1

--- git-changelog.rb
+++ git-changelog.rb
@@ -1,9 +1,9 @@
 class GitChangelog < Formula
-  version "0.3.0"
+  version "0.3.1"
   desc "Categorized change logs from git messages"
   homepage "https://github.com/aldrin/git-changelog"
-  url "https://github.com/aldrin/git-changelog/releases/download/v0.3.0/git-changelog-v0.3.0-osx.tar.gz"
-  sha256 "fa5b660bb2947bbb186b69848ec9e97af5498b8ac30bde252fbf7f521a6327b2"
+  url "https://github.com/aldrin/git-changelog/releases/download/v0.3.1/git-changelog-v0.3.1-osx.tar.gz"
+  sha256 "a3057e3c634ba1c4ac76ce5cb48ccff08d5a65775bb2a04fbeee7b636eed3c68"
 
   def install
     bin.install "git-changelog"
